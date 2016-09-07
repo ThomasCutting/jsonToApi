7731bcee5b77c82b22aa85c7e0981f32afc41b04
Thomas Cutting <tom21212@gmail.com>
2016-09-07 00:31:18 -0700

    Completed the user api layer.

--- routes/api.php
+++ routes/api.php
@@ -2,5 +2,12 @@
 
 use Illuminate\Http\Request;
 
+/**
+ * Party Layer == >Third< Party Layer that is controlled for all users.
+ */
 Route::get('/{token}','PartyLayerController@parseGet');
+Route::post('/{token}','PartyLayerController@parsePost');
 Route::get('/{token}/{unique}/{query}','PartyLayerController@parseViaUnique');
+Route::put('/{token}/{unique}/{query}','PartyLayerController@updateViaUnique');
+Route::patch('/{token}/{unique}/{query}','PartyLayerController@updateViaUnique');
+Route::delete('/{token}/{unique}/{query}','PartyLayerController@deleteViaUnique');
