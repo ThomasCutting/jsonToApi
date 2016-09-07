4c382b109b94cb760fbcac37dc621e7a780e1f8f
Thomas Cutting <tom21212@gmail.com>
2016-09-07 13:54:15 -0700

    Updated the metadata for jsontoapi.com

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,11 +1,54 @@
-<html>
+<html xmlns="http://www.w3.org/1999/xhtml"
+      xmlns:og="http://opengraphprotocol.org/schema/"
+      xmlns:fb="http://www.facebook.com/2008/fbml"
+      xml:lang="en"
+      lang="en"
+      style="font-size: 9.81px;">
 
     <head>
+
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible"
+              content="IE=edge; chrome=1">
+        <title>
+            JSON 2 API | Turn your JSON objects into a RESTful API
+        </title>
+        <meta name="viewport"
+              content="width=device-width, initial-scale=1">
+        <meta name="description"
+              content="Easily turn your JSON objects into a RESTful API.">
+        <meta name="keywords"
+              content="json to api, json api cloud, json to cloud, turn json into an api, turn json into api, how to make a api from json, how to make an api from json, how do i turn json into an api">
+        <meta name="author"
+              content="Thomas Cutting">
+        <meta name="google-site-verification"
+              content="hkuPgHszDR7GrWOoygSaFg6RQv1FZsHvyot-bo6RrOY">
+        <!-- -->
+        <link rel="shortcut icon"
+              href="/img/favicon.ico"
+              type="image/x-icon">
+        <!-- -->
+        <meta property="og:title"
+              content="JSON 2 API | Turn your JSON objects into a RESTful API">
+        <meta property="og:url"
+              content="http://jsontoapi.com">
+        <meta property="og:image"
+              content="http://jsontoapi.com/img/json2apiimage.png">
+        <meta property="og:site_name"
+              content="JSON 2 API">
+        <meta property="og:description"
+              content="Easily turn your JSON objects into a RESTful API.">
+        <!-- -->
+        <link rel="alternate" hreflang="en" href="http://jsontoapi.com">
+        <link rel="alternate" hreflang="en-gb" href="http://jsontoapi.com">
+
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.4/css/bootstrap.min.css" integrity="2hfp1SzUoho7/TsGGGDaFdsuuDL0LX2hnUp6VkX3CUQ2K4K+xjboZdsXyp4oUHZj" crossorigin="anonymous">
         <script src="https://code.jquery.com/jquery-3.1.0.min.js"></script>
         <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.4/js/bootstrap.min.js" integrity="VjEeINv9OSwtWFLAtmc4JCtEJXXBub00gtSnszmspDLCtC0I4z4nqz7rEFbIZLLU" crossorigin="anonymous"></script>
         <link href="https://fonts.googleapis.com/css?family=Montserrat:400,700|Open+Sans:300,400,600,700,800" rel="stylesheet">
         <link href="{{url('css/app.css')}}" rel="stylesheet">
+
+
     </head>
 
     <body>
