b002b6587febc810ba643b64f7829adb03554214
Thomas Cutting <tom21212@gmail.com>
2016-09-07 00:33:45 -0700

    Added some comments.

--- routes/api.php
+++ routes/api.php
@@ -5,9 +5,18 @@ use Illuminate\Http\Request;
 /**
  * Party Layer == >Third< Party Layer that is controlled for all users.
  */
+// Fetch all entities within your DocumentSchema
 Route::get('/{token}','PartyLayerController@parseGet');
+
+// Create a new DocumentEntity
 Route::post('/{token}','PartyLayerController@parsePost');
+
+// Find BY query.
 Route::get('/{token}/{unique}/{query}','PartyLayerController@parseViaUnique');
+
+// Update BY query.
 Route::put('/{token}/{unique}/{query}','PartyLayerController@updateViaUnique');
 Route::patch('/{token}/{unique}/{query}','PartyLayerController@updateViaUnique');
+
+// Remove BY query.
 Route::delete('/{token}/{unique}/{query}','PartyLayerController@deleteViaUnique');
